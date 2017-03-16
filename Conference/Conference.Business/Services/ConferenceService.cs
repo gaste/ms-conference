b95d37329709b4d19e7e102be99d2dbf750c049f
UliA <UlrichAckermann@hotmail.com>
2017-03-16 15:44:39 +0100

    Add ConferenceService

--- Conference/Conference.Business/Services/ConferenceService.cs
+++ Conference/Conference.Business/Services/ConferenceService.cs
@@ -1,4 +1,5 @@
 ﻿using Conference.Business.Interfaces;
+using Conference.Data.DbContext;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,29 +10,73 @@ namespace Conference.Business.Services
 {
     public class ConferenceService : IService<Data.Entities.Conference, Guid>
     {
+        private IConferenceDbContextFactory factory;
+
+        private ConferenceService() { }
+        public ConferenceService(IConferenceDbContextFactory factory)
+        {
+            this.factory = factory;
+        }
+
         public void Add(Data.Entities.Conference newItem)
         {
-            throw new NotImplementedException();
+            using (IConferenceDbContext db = factory.Create())
+            {
+                db.Conferences.Add(newItem);
+                db.SaveChanges();
+            }
         }
 
         public void Delete(Data.Entities.Conference deleteItem)
         {
-            throw new NotImplementedException();
+            using (IConferenceDbContext db = factory.Create())
+            {
+                Data.Entities.Conference conference = db.Conferences
+                                .Where(c => c.ConferenceId == deleteItem.ConferenceId)
+                                .FirstOrDefault();
+                db.Conferences.Remove(conference);
+                db.SaveChanges();
+            }
         }
 
         public IEnumerable<Data.Entities.Conference> GetItems()
         {
-            throw new NotImplementedException();
+            IEnumerable<Data.Entities.Conference> conferences = null;
+            using (IConferenceDbContext db = factory.Create())
+            {
+                conferences = db.Conferences;
+            }
+
+            return conferences;
         }
 
-        public Data.Entities.Conference GetSingleItemByID(Guid id)
+        public Data.Entities.Conference GetSingleItemByID(Guid conferenceId)
         {
-            throw new NotImplementedException();
+            Data.Entities.Conference conference = null;
+            using (IConferenceDbContext db = factory.Create())
+            {
+                conference = db.Conferences.Where(l => l.ConferenceId == conferenceId)
+                    .FirstOrDefault();
+            }
+
+            return conference;
         }
 
         public void Update(Data.Entities.Conference changedItem)
         {
-            throw new NotImplementedException();
+            using (IConferenceDbContext db = factory.Create())
+            {
+                Data.Entities.Conference conference = db.Conferences.Where(l => l.ConferenceId == changedItem.ConferenceId)
+                    .FirstOrDefault();
+
+                conference.Abstract = changedItem.Abstract;
+                conference.AmountOfLastingDays = changedItem.AmountOfLastingDays;
+                conference.Location = changedItem.Location;
+                conference.StartsAt = changedItem.StartsAt;
+                conference.Title = changedItem.Title;
+
+                db.SaveChanges();
+            }
         }
     }
 }
