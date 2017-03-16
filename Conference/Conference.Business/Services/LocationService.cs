22aafe0a573f7cd791e19a060d7836462cb50729
UliA <UlrichAckermann@hotmail.com>
2017-03-16 15:25:59 +0100

    ..

--- Conference/Conference.Business/Services/LocationService.cs
+++ Conference/Conference.Business/Services/LocationService.cs
@@ -66,7 +66,7 @@ namespace Conference.Business.Services
 
         public void Update(Location changedItem)
         {
-            
+            throw new NotImplementedException();
         }
     }
 }
